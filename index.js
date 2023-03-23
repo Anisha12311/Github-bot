09127a45eceb57206baad4b9eccbb260a53fb6f5
Anisha Yadav <2000anishayadav@gmail.com>
2023-03-23 14:12:21 +0530

    converts dates 2022-07-25T14:07:07+05:30

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
+
 const jsonfile = require('jsonfile')
 const filepath = './data.json';
+const simpleGit = require('simple-git')
 const moment = require('moment')
 
-const DATE = moment().format();
+const makeCommit = n => {
+    if(n===0) return simpleGit().push();
+ const x =  Math.floor(Math.random() * (54 - 0 + 1)) + 0;
+ const y =Math.floor(Math.random() * (6 - 0 + 1)) + 0;
+ console.log("x",x)
+ console.log("y",y)
+const DATE = moment().subtract(1,'y').add(1,'d').add(x,'w').add(y,'d').format();
 const data = {
     date : DATE
 }
+
 console.log(DATE)
 
-jsonfile.writeFile(filepath,data)
+jsonfile.writeFile(filepath,data,() => {
+    simpleGit().add([filepath]).commit(DATE,{'--date': DATE},makeCommit.bind(this,--n)).push()
+})
+}
+makeCommit(50)
